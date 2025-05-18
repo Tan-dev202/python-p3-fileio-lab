b4658f9b0b9b82e1af86ba4a5d9e1063d5fc087d
Enock Tangus <tanguskipngeno@gmail.com>
2025-05-18 13:30:28 +0300

    fileio lab complete

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,14 @@
-def write_file(file_name, file_content):
-    pass
+def write_file(file_name='fruit_basket', file_content='Log 1: 6 apples added'):
+    with open(f'{file_name}.txt',  mode='w', encoding='utf-8') as file_name:
+        file_name.write(file_content)
+    
 
-def append_file(file_name, append_content):
-    pass
+def append_file(file_name='fruit_basket', append_content='Log 2: 3 pears removed'):
+    with open(f'{file_name}.txt',  mode='a', encoding='utf-8') as file_name:
+        file_name.write(append_content)
+    
 
-def read_file(file_name):
-    pass
+def read_file(file_name='fruit_basket'):
+    with open(f'{file_name}.txt',  mode='r', encoding='utf-8') as file_name:
+        return file_name.read()
+    
